c379d14f3fd539b9c454f99585071353267e0279
dimnikolos <dnikolos@gmail.com>
2018-04-22 17:00:09 +0300

    add formatted address

--- WhereisX.js
+++ WhereisX.js
@@ -9,18 +9,6 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.latitudeOf = function(location,callback){
-      $.ajax({
-         url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBX9t97s78v5FrCMpnuwZZWYT2vcpf6POU',
-         dataType: 'json',
-         success: function( geoData ) {
-         // Got the data - parse it and return the temperature
-         console.log(geoData);
-         var lat = geoData['results'][0]['geometry']['location']['lat'];
-         callback(lat);
-         }
-         });
-    };
 
     ext.longitudeOf = function(location,callback){
       $.ajax({
@@ -35,11 +23,35 @@
     };
 
 
+    ext.latitudeOf = function(location,callback){
+      $.ajax({
+         url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBX9t97s78v5FrCMpnuwZZWYT2vcpf6POU',
+         dataType: 'json',
+         success: function( geoData ) {
+         // Got the data - parse it and return the temperature
+         var lat = geoData['results'][0]['geometry']['location']['lat'];
+         callback(lat);
+         }
+         });
+    };
+
+    ext.formattedAdress = function(location,callback){
+      $.ajax({
+         url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBX9t97s78v5FrCMpnuwZZWYT2vcpf6POU',
+         dataType: 'json',
+         success: function( geoData ) {
+         // Got the data - parse it and return the temperature
+         var lat = geoData['results'][0]['formatted_address'];
+         callback(lat);
+         }
+         });
+  	};
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['R', 'longitude of %s', 'longitudeOf', 'Athens, Greece'],
             ['R', 'latitude of %s', 'latitudeOf', 'Athens, Greece'],
+            ['R', 'Formatted address of %s', 'formattedAdress', 'Athens'],
         ]
     };
 
