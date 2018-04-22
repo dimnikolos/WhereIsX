8237aec9a5515f96526c121fc54c57c844fcdeef
dimnikolos <dnikolos@gmail.com>
2018-04-22 17:09:13 +0300

    add cache!

--- WhereisX.js
+++ WhereisX.js
@@ -1,5 +1,14 @@
 //https://maps.googleapis.com/maps/api/geocode/json?address=Arta,%20Greece&key=AIzaSyBX9t97s78v5FrCMpnuwZZWYT2vcpf6POU
 (function(ext) {
+      var cacheDuration = 1800000 //ms, 30 minutes
+      var cachedWhere = {};    
+      class whereData{
+          constructor(lng,lat,form){
+              this.lng = lng;
+              this.lat = lat;
+              this.form = form;
+          };
+      };
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -11,12 +20,22 @@
 
 
     ext.longitudeOf = function(location,callback){
+        //if in cache
+        if (location in cachedWhere &&
+            Date.now() - cachedWhere[location].time < cacheDuration) {
+          //Weather data is cached
+          callback(cachedWhere[location].lng);
+          return;
+        }
       $.ajax({
          url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBX9t97s78v5FrCMpnuwZZWYT2vcpf6POU',
          dataType: 'json',
          success: function( geoData ) {
          // Got the data - parse it and return the temperature
          var lng = geoData['results'][0]['geometry']['location']['lng'];
+         var lat = geoData['results'][0]['geometry']['location']['lat'];
+         var form = geoData['results'][0]['formatted_address'];
+         cachedWhere[location] = whereData(lng,lat,form);
          callback(lng);
          }
          });
@@ -24,28 +43,46 @@
 
 
     ext.latitudeOf = function(location,callback){
+        if (location in cachedWhere &&
+            Date.now() - cachedWhere[location].time < cacheDuration) {
+          //Weather data is cached
+          callback(cachedWhere[location].lat);
+          return;
+        }
       $.ajax({
          url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBX9t97s78v5FrCMpnuwZZWYT2vcpf6POU',
          dataType: 'json',
          success: function( geoData ) {
          // Got the data - parse it and return the temperature
+         var lng = geoData['results'][0]['geometry']['location']['lng'];
          var lat = geoData['results'][0]['geometry']['location']['lat'];
+         var form = geoData['results'][0]['formatted_address'];
+         cachedWhere[location] = whereData(lng,lat,form);
          callback(lat);
          }
          });
     };
 
     ext.formattedAdress = function(location,callback){
+        if (location in cachedWhere &&
+            Date.now() - cachedWhere[location].time < cacheDuration) {
+          //Weather data is cached
+          callback(cachedWhere[location].lat);
+          return;
+        }        
       $.ajax({
          url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBX9t97s78v5FrCMpnuwZZWYT2vcpf6POU',
          dataType: 'json',
          success: function( geoData ) {
          // Got the data - parse it and return the temperature
-         var lat = geoData['results'][0]['formatted_address'];
-         callback(lat);
+         var lng = geoData['results'][0]['geometry']['location']['lng'];
+         var lat = geoData['results'][0]['geometry']['location']['lat'];
+         var form = geoData['results'][0]['formatted_address'];
+         cachedWhere[location] = whereData(lng,lat,form);
+         callback(form);
          }
          });
-  	};
+      };
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
